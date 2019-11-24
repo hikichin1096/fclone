20c506f7ba00410ba765ba2d4070272ab93d5227
Wataru Hikichi <w.hikichi.kg@gmail.com>
2019-11-24 17:10:59 +0900

    ストロングパラメータ対応

--- app/controllers/facclones_controller.rb
+++ app/controllers/facclones_controller.rb
@@ -7,6 +7,15 @@ class FacclonesController < ApplicationController
   end
 
   def create
-    @facclone = Facclone.create(content: params[:facclone][:content])
+    #@facclone = Facclone.create(content: params[:facclone][:content])
+    Facclone.create(facclone_params)
+    redirect_to new_facclone_path
   end
+
+  private
+
+  def facclone_params
+    params.require(:facclone).permit(:content)
+  end
+
 end
